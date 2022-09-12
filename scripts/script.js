b96a5fc20118c8518d159965f5c15c313271c022
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2022-09-11 21:37:35 -0300

    11/09/2022

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,28 @@
 
 function scrollTecnologias () {
-    window.scroll(0 , 1750)
-  }
+  window.scroll(0 , 1750)
+};
 
 function scrollObjetivos () {
-    window.scroll(0 , 1000)
-  }
+  window.scroll(0 , 1000)
+ };
 
 function scrollObjetivos () {
-    window.scroll(0 , 1000)
-  }
+  window.scroll(0 , 1000)
+};
 
 function scrollContatos () {
-    window.scroll(0, 100000)
-}
+ window.scroll(0, 100000)
+};
+
+function scrollToStart () {
+  window.scroll(0, 0)
+};
+
+function scrollTrilha () {
+  window.scroll(0, 3500)
+};
+
+function scrollProjetos () {
+  window.scroll(0, 4000)
+};
